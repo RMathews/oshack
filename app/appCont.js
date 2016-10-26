3f74afa5c2f798f0ccaa9bee807718662e6af2c9
Revon Mathews <revon.mathews@intel.com>
2016-10-26 13:39:15 -0500

    you-me chat bubble

--- app/appCont.js
+++ app/appCont.js
@@ -3,21 +3,28 @@
  */
 var app1 = angular.module('app1', []);
 
-app1.controller('appCtrl', function($scope) {
+app1.controller('contentController', function($scope) {
 
     $scope.userMsg = "Help spin VM";
+    $scope.botMsg = "Help";
     // Message inbox
     $scope.messages = [];
+
 
     $scope.recieveMsg = function(){
         var msg = {};
         msg.time = new Date().getTime();
+        msg.sender = 'you';
         msg.text = $scope.userMsg;
         $scope.messages.push(msg);
 
     };
 
     $scope.sendMsg = function(){
-        $scope.sentMsg = $scope.userMsg;
+        var msg = {};
+        msg.time = new Date().getTime();
+        msg.sender = 'me';
+        msg.text = $scope.botMsg;
+        $scope.messages.push(msg);
     };
 });
