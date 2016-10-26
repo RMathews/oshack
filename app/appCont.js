51ff6436c2b8eca68982ec189259ae10cd4dea31
Revon Mathews <revon.mathews@intel.com>
2016-10-26 18:22:09 -0500

    some changes

--- app/appCont.js
+++ app/appCont.js
@@ -5,8 +5,9 @@ var app1 = angular.module('OSHack', []);
 
 app1.controller('contentController', function($scope) {
 
-    $scope.userMsg = "Help spin VM";
-    $scope.botMsg = "Help";
+    $scope.userMsg = "Angular team meeting notes for this week: Yet more new team members on Material, Universal in core, going to China.";
+    $scope.botMsg = "AngularJS is a structural framework for dynamic web apps. It lets you use HTML " +
+        "as your template language and lets you extend HTML's syntax to express your application's components clearly and succinctly. ";
     // Message inbox
     $scope.messages = [];
 
@@ -27,4 +28,9 @@ app1.controller('contentController', function($scope) {
         msg.text = $scope.botMsg;
         $scope.messages.push(msg);
     };
+
+    $scope.getList = function(){
+      return $scope.showList ? "unhistory.html" : "history.html";
+    };
 });
+
