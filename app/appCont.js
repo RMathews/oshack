c8995112c64a5c9b763582fc46c8ec04431ade88
Revon Mathews <revon.mathews@intel.com>
2016-10-28 10:30:27 -0500

    added send button, removed init msg

--- app/appCont.js
+++ app/appCont.js
@@ -32,9 +32,8 @@ app1.directive('scrollToLast', ['$location', '$anchorScroll', function($location
 
 app1.controller('contentController', ['$scope','$http', function($scope, $http) {
 
-    $scope.userMsg = "Angular team meeting notes for this week: Yet more new team members on Material, Universal in core, going to China.";
-    $scope.botMsg = "AngularJS is a structural framework for dynamic web apps. It lets you use HTML " +
-        "as your template language and lets you extend HTML's syntax to express your application's components clearly and succinctly. ";
+    $scope.userMsg = "";
+    $scope.botMsg = "";
     // Message inbox
     $scope.messages = [];
 
@@ -43,16 +42,14 @@ app1.controller('contentController', ['$scope','$http', function($scope, $http) 
     };
 
     $scope.setAPI = function(type, value){
-        var url = "http://172.99.106.89:8080/set?key=flavor&value=m1.large";
+        var url = "http://172.99.106.89:8080/set?key="+type+"&value="+value;
         var data = $http.jsonp(url)
             .success(function(response){
                 console.log("success");
-                console.log(response);
-                // $scope.sendMsg(response);
+                $scope.sendMsg(response);
             }).error(function(response){
                 console.log("failure");
             });
-        $scope.userMsg = "";
     };
 
     $scope.recieveMsg = function(userMsg){
@@ -64,12 +61,10 @@ app1.controller('contentController', ['$scope','$http', function($scope, $http) 
         msg.text = {message: userMsg, list:[], button:'False'};
         $scope.messages.push(msg);
 
-        var url = "http://172.99.106.89:8080/chat?question=create%20vm";
+        var url = "http://172.99.106.89:8080/chat?question="+userMsg;
 
         var data = $http.jsonp(url)
             .success(function(response){
-            console.log("success");
-            console.log(response);
             $scope.sendMsg(response);
         }).error(function(response){
             console.log("failure");
