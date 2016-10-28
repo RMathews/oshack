d00c55edaa37fc18496cc343971abfb054e3c988
Revon Mathews <revon.mathews@intel.com>
2016-10-28 01:05:46 -0500

    set

--- app/appCont.js
+++ app/appCont.js
@@ -30,13 +30,30 @@ app1.directive('scrollToLast', ['$location', '$anchorScroll', function($location
     };
 }]);
 
-app1.controller('contentController', function($scope, $http) {
+app1.controller('contentController', ['$scope','$http', function($scope, $http) {
 
     $scope.userMsg = "Angular team meeting notes for this week: Yet more new team members on Material, Universal in core, going to China.";
     $scope.botMsg = "AngularJS is a structural framework for dynamic web apps. It lets you use HTML " +
         "as your template language and lets you extend HTML's syntax to express your application's components clearly and succinctly. ";
     // Message inbox
     $scope.messages = [];
+
+    $scope.callback = function(data){
+        alert(data);
+    };
+
+    $scope.setAPI = function(type, value){
+        var url = "http://172.99.106.89:8080/set?key=flavor&value=m1.large";
+        var data = $http.jsonp(url)
+            .success(function(response){
+                console.log("success");
+                console.log(response);
+                // $scope.sendMsg(response);
+            }).error(function(response){
+                console.log("failure");
+            });
+        $scope.userMsg = "";
+    };
 
     $scope.recieveMsg = function(userMsg){
         var msg = {};
@@ -46,32 +63,27 @@ app1.controller('contentController', function($scope, $http) {
         msg.sender = 'user';
         msg.text = {message: userMsg, list:[], button:'False'};
         $scope.messages.push(msg);
-        console.log('called')
-        // jQuery cross domain ajax
-        $.get("http://172.99.106.89:8080/chat?question="+userMsg).done(function (data) {
-             console.log(data);
+
+        var url = "http://172.99.106.89:8080/chat?question=create%20vm";
+
+        var data = $http.jsonp(url)
+            .success(function(response){
+            console.log("success");
+            console.log(response);
+            $scope.sendMsg(response);
+        }).error(function(response){
+            console.log("failure");
         });
+        $scope.userMsg = "";
     };
 
-    $scope.sendMsg = function(){
+    $scope.sendMsg = function(data){
         var msg = {};
-        if($scope.botMsg.length <= 0)
+        if(data.length <= 0)
             return;
         msg.time = new Date().getTime();
         msg.sender = 'bot';
-        msg.text = {message: "Sure. Creating VM. But please select a flavor",
-                    list:[{"value": "m1.tiny", "type": "flavor"},
-                        {"value": "m1.small", "type": "flavor"},
-                        {"value": "m1.medium", "type": "flavor"},
-                        {"value": "m1.large", "type": "flavor"},
-                        {"value": "m1.nano", "type": "flavor"},
-                        {"value": "m1.xlarge", "type": "flavor"},
-                        {"value": "m1.micro", "type": "flavor"}],
-                    button:"True"}
+        msg.text = data;
         $scope.messages.push(msg);
     };
-
-    $scope.getList = function(){
-        return $scope.showList ? "unhistory.html" : "history.html";
-    };
-});
+}]);
